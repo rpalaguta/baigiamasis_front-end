1f81a9452c5fee7cccbb56b08f88b260d4333c0f
Raimondas Palaguta <r.palaguta@gmail.com>
2022-08-15 00:44:10 +0300

    Implemented routes filtering and fixed auth

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,14 @@
-import React from "react";
-import './layout.css'
-import NavBar from "../NavBar/NavBar";
-import Content from "./Content";
+import React from 'react';
+import './layout.css';
+import NavBar from '../NavBar/NavBar';
+import Content from './Content';
 
 export default function Layout() {
-    return (
-        <div className="layout">
-            <NavBar />
-            <Content />
-        </div>
-    )
+  // padaryt useEffect jei nėra user, bet yra token - parodyt spinnerį
+  return (
+    <div className="layout">
+      <NavBar />
+      <Content />
+    </div>
+  );
 }
--- src/services/httpClient.js
+++ src/services/httpClient.js
@@ -1,27 +1,24 @@
-import axios from "axios";
+import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost/api';
 
 //vyksta pries requesta
-axios.interceptors.request.use(config => {
-    const user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : undefined;
-    // const user = useSelector((state) => state.user.value) ? JSON.parse(useSelector((state) => state.user.value)) : undefined;
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
 
-    if(user) {
-        const token = `Bearer ${user.token}`;
-        config.headers.Authorization = token;
-    }
-
-    return config;
-
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
-export const setAuthToken = (token) =>  {
-    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-}
+export const setAuthToken = (token) => {
+  console.log({ token });
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+};
 
 export const clearAuthToken = () => {
-    delete axios.defaults.headers.common["Authorization"];
-}
+  delete axios.defaults.headers.common['Authorization'];
+};
 
 export default axios;
